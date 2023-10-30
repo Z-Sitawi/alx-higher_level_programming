b136e58290614b9b7edc29ab0856bd2048b9f20e
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-10-30 06:59:38 +0000

    add 0x00

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+import sys
+error = "and that piece of art is useful - Dora Korpar, 2015-10-19"
+sys.stderr.write(error + "\n")
+exit(1)
---
+++ 0x00-python-hello_world/README.md
@@ -0,0 +1,3 @@
+# 0x00. Python - Hello, World
+This is higher level programming.
+We will use Python.
