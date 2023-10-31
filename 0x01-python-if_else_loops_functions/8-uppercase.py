9f1a80410280606334e5bfdf3f7cd2e7cb2629e5
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-10-31 18:18:20 +0000

    task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 def uppercase(str):
+    """Print a string in uppercase."""
+    for c in str:
+        if 97 <= ord(c) <= 122:  # if lowercase
+        c = chr(ord(c) - 32)  # get ascii of upper & turn it to char
+        print("{}".format(c), end="")
+    print("")
+
+# while loop version
+"""
+def uppercase(str):
     i = 0
     s = ""
     while i < len(str):
@@ -13,3 +23,4 @@ def uppercase(str):
             i += 1
 
     print("{:s}".format(s))
+"""
