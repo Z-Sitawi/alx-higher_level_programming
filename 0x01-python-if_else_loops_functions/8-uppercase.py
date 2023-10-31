4caaa03819d4090c4c14b4a83b61cbc38084d382
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-10-31 18:19:00 +0000

    task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -6,21 +6,3 @@ def uppercase(str):
         c = chr(ord(c) - 32)  # get ascii of upper & turn it to char
         print("{}".format(c), end="")
     print("")
-
-# while loop version
-"""
-def uppercase(str):
-    i = 0
-    s = ""
-    while i < len(str):
-        c = str[i]
-        if 97 <= ord(c) <= 122:  # if lowercase
-            ac = ord(c) - 32  # get ascii of uppercase
-            c = chr(ac)  # turn ascii to character
-            s = s + c  # add character to string.
-        else:
-            s = s + c
-            i += 1
-
-    print("{:s}".format(s))
-"""
