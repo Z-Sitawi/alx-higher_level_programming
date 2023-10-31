9a5077596365566dae63252aa984f31664f304f1
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-10-31 18:20:49 +0000

    task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,6 @@ def uppercase(str):
     """Print a string in uppercase."""
     for c in str:
         if 97 <= ord(c) <= 122:  # if lowercase
-        c = chr(ord(c) - 32)  # get ascii of upper & turn it to char
+            c = chr(ord(c) - 32)  # get ascii of upper & turn it to char
         print("{}".format(c), end="")
     print("")
