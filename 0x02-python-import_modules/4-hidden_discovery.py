5329e1130862e0207790b36266f197e71d7c7eb6
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-11-02 14:47:31 +0100

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     import hidden_4
 
     names_list = dir(hidden_4)
-     for name in names_list:
+    for name in names_list:
         if name[:2] != "__":
             print(name)
