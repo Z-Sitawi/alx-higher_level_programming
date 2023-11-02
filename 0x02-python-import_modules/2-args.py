5563819ea1d4a22fc08dafe0e277a94c14145f5f
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-11-02 10:47:34 +0100

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+if __name__ == '__main__':
+    """Print the number of and list of arguments."""
+    import sys
+    argc = len(sys.argv) - 1
+    argv = sys.argv
+    count = 0
+
+    if not(argc > 0):
+        print("{:d} arguments.".format(argc))
+    else:
+        print("{:d} arguments:".format(argc))
+        for x in argv:
+            if x not in argv[0]:
+                count = count + 1
+                print("{:d}: {:s}".format(count, x))
