656efdc010c0cd9fed53371415d3af3ef35c7aa7
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-11-04 17:20:43 +0100

    task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def no_c(my_string):
+    """ removes all characters c and C from a string """
+    new_str = ""
+    for x in my_string:
+        if x == "c" or x == "C":
+            x = ""
+
+        new_str = new_str + x
+    return new_str
