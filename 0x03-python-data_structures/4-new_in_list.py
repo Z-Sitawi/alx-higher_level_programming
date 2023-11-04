f08524bf7e61a20aaf62bcc778720b75ae763cb1
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-11-04 16:35:11 +0100

    task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    """ replaces an element in a list at a specific position without modifying the original list"""
+    list_copy = []
+    if isinstance(my_list, list):
+        list_copy = my_list.copy()
+    if idx < 0:
+        return list_copy
+    elif idx >= len(my_list):
+        return list_copy
+    else:
+        list_copy[idx] = element
+        return list_copy
