c93cbc26beb7d672baced17d17d557cbd9245f8c
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-11-04 18:08:14 +0100

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     """ prints a matrix of integers """
     for x in matrix:
-        for i in x:
-            print("{:d}".format(i), end=" ")
-        print("")
+        for i in range(len(x)):
+            print("{:d}".format(x[i]), end=" " if i != len(x) - 1 else "")
+
+        print("$")
