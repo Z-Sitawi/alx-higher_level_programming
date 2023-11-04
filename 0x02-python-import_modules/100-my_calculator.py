c9ab59bd2daf3b7226204950ba79078465584b03
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-11-04 18:49:04 +0100

    task 100

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    """a simple calculator that handles basic operations """
+    from calculator_1 import add, sub, div, mul
+    import sys
+
+    operators = "+-*/"
+    argv = sys.argv
+    argc = len(argv)
+    a = int(argv[1])
+    b = int(argv[3])
+    if argc < 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    if argv[2] == "+":
+        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    elif argv[2] == "-":
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    elif argv[2] == "*":
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    elif argv[2] == "/":
+        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    exit(0)
