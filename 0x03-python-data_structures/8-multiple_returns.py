a07b99c571c777d4b083d82fd4ef91b611bd6137
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-11-04 23:12:38 +0100

    task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def multiple_returns(sentence):
+    """ returns a tuple with the length of a string and its first char """
+    str_len = len(sentence)
+    if sentence is None or sentence == "":
+        return 0, None
+    else:
+        return str_len, sentence[0]
