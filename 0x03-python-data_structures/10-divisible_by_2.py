1a703093a2ab3b840eec64f0a8f934049e4c4998
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-11-05 00:03:53 +0100

    task 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    """ return multiples of 2 in a list as a list of tuples."""
+    results = []
+
+    for x in my_list:
+        if x % 2 == 0:
+            results.append(True)
+        else:
+            results.append(False)
+    return results
