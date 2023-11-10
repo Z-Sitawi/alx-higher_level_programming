c40c1ab1c2c014a1bfb0eb8eabe7e4f0fb283fd7
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-11-10 23:08:19 +0100

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     """ returns a key with the biggest integer value """
-    if isinstance(a_dictionary, dict) or len(a_dictionary) != 0:
-        # get value of the first key.
-        key = list(a_dictionary.keys())[0]
-        big = a_dictionary[key]
-        # compare it with the rest values.
-        for k, v in a_dictionary.items():
-            if v > big:
-                big = v
-                key = k
-        return key
-    else:
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
+
+    # get value of the first key.
+    key = list(a_dictionary.keys())[0]
+    big = a_dictionary[key]
+    # compare it with the rest values.
+    for k, v in a_dictionary.items():
+        if v > big:
+            big = v
+            key = k
+    return key
