4f799842cb91c9ecadebec76981de7f351acf76e
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-11-10 21:37:58 +0100

    task 6

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     """ prints a dictionary by ordered keys """
-    key, val = [], []
-    for x in a_dictionary.keys():
-        key.append(x)
-    for y in a_dictionary.values():
-        val.append(y)
-    i = 0
-    for z in range(len(key) - 1):
-        print("{}: {}".format(key[z], val[z]))
-
-
-a_dictionary = {'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3]}
-print_sorted_dictionary(a_dictionary)
+    keys = []
+    for x, y in a_dictionary.items():
+        keys.append("{}: {}".format(x, y))
+        keys.sort()
+    for i in keys:
+        print(i)
