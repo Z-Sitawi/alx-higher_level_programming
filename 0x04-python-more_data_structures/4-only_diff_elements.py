12d65c755cad12c78781f1b2bb97d0cf2d31035b
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-11-10 22:07:57 +0100

    task 4

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
     """  returns a set of all elements present in only one set """
+    elements = set()
+    for x in set_1:
+        if x not in set_2 and x not in elements:
+            elements.add(x)
+    for y in set_2:
+        if y not in set_1 and y not in elements:
+            elements.add(y)
+
+    return elements
