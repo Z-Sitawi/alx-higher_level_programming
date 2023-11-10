801abfa29dcb97b438d1516a49adf3e1c749dad5
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-11-10 22:35:20 +0100

    task 8

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    if not (key in a_dictionary) or key == "":
+    """ deletes a key in a dictionary
+        key argument will be always a string
+        If a key doesn’t exist, the dictionary won’t change
+    """
+    if isinstance(a_dictionary, dict):
+        if key in a_dictionary:
+            del a_dictionary[key]
+            return a_dictionary
         return a_dictionary
-    del a_dictionary[key]
-    return a_dictionary
