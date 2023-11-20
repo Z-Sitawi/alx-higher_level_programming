fe61963fcea6dab04ece50ae6735e597d17930e8
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-11-20 18:14:22 +0100

    task 6

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def raise_exception_msg(message=""):
     """ raises a name exception with a message """
-    print("{:s}".format(message))
-    raise NameError
+    raise NameError(message)
