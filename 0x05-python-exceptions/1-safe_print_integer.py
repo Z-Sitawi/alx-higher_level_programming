6d970ec69cde4530e6a2a7e6d37c2366eaf76f89
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-11-20 14:12:47 +0100

    task 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    if isinstance(value, int):
-        try:
+    try:
+        if isinstance(value, int):
             print("{:d}".format(value))
             return True
-        except ValueError:
-            return False
+    except ValueError:
+        return False
