702d0a9e7a1338f34bffe38ba2ea8753cac281d2
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-11-21 11:28:06 +0100

    task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """ Represent a square"""
+    def __init__(self, size=0):
+        if isinstance(size, int):
+            self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size ** 2
