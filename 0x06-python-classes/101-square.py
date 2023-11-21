f5e71decdb55e47f0f7f15bc9ed6c6d4c870fb67
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-11-21 19:44:03 +0100

    task 101

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -12,8 +12,8 @@ class Square:
             size (int): The size of the new square.
             position (int, int): The position of the new square.
         """
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
@@ -63,16 +63,11 @@ class Square:
 
     def __str__(self):
         """Define the print() representation of a Square."""
-        if self.__size == 0:
-            print("")
-            return
-
-        for x in range(0, self.__position[1]):
-            print("")
+        if self.__size != 0:
+            [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
-            for j in range(0, self.__position[0]):
-                print(" ", end="")
-            for k in range(0, self.__size):
-                print("#", end="")
-            print("")
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            if i != self.__size - 1:
+                print("")
         return ""
