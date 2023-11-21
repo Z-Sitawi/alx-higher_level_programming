2deff161962ca7c81cf95ae7abd276271c60215e
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-11-21 21:28:19 +0100

    task 103

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -11,7 +11,7 @@ class MagicClass:
             Arg:
                radius (float or int): The radius of the new circle.
        """
-        if type(radius) is not int or type(radius) is not float:
+        if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
         self.__radius = radius
 
