ce403fa6a1651fcf71e3b51dca5d2ba9cf38393b
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-11-27 23:03:38 +0100

    task 3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -31,3 +31,23 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    def area(self):
+        """ :return: the rectangle area """
+        return self.width * self.height
+
+    def perimeter(self):
+        """ :return: the rectangle perimeter """
+        if self.width == 0 or self.height == 0:
+            return 0
+        else:
+            return (self.width + self.height) * 2
+
+    def __str__(self):
+        if self.width == 0 or self.height == 0:
+            return ""
+        for y in range(self.height):
+            for x in range(self.width):
+                print("#", end="")
+            print("", end="\n" if y < self.height - 1 else "")
+        return ""
