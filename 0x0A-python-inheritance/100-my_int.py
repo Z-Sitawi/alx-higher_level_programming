2aedd2bec734cb3aa34fe6ae95623b9758958d14
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-12-04 21:29:14 +0100

    task 100

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""Defines a class 'MyInt' that inherits from 'int' """
+
+
+class MyInt(int):
+    """ Inverts == and != operators """
+
+    def __eq__(self, other):
+        """ reverts == to !=
+        :param other: an integer
+        """
+        return self.real != other
+
+    def __ne__(self, other):
+        """ reverts != to ==
+        :param other: an integer
+        """
+        return self.real == other
