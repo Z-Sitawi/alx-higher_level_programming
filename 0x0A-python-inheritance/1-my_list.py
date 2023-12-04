34cae52b48430f7577d5ba78ecd6dde83db43bab
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-12-04 14:07:11 +0100

    task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,8 +4,6 @@
 
 class MyList(list):
     """ Defines a list """
-    def __init__(self):
-        super().__init__()
 
     def print_sorted(self):
         """ prints the list, but sorted (ascending sort) """
