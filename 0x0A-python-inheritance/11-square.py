4efd62256d9fe173f2b26ce3e9a28aab9098f16c
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-12-04 21:15:54 +0100

    task 11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+""" Defines a class Square that inherits from Rectangle"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Represent a Square using Rectangle."""
+    def __init__(self, size):
+        """
+            Initialize a Square
+        :param size: size of the square
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """ calculates the area of a square """
+        return self.__size * self.__size
+
+    def __str__(self):
+        return f"[Square] {self.__size}/{self.__size}"
