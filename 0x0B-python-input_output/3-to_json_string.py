8cdb03be2769765752543a4464b21fd8bfff6512
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-12-06 00:17:19 +0100

    task 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+""" a function that returns the JSON representation of an object (string) """
+import json
+
+
+def to_json_string(my_obj):
+    """
+
+    :param my_obj: an object
+    :return: the JSON representation of an object (string)
+    """
+    return json.dumps(my_obj)
