541b6486d568a5bf4451d3f99347873060f9b791
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-12-06 17:54:33 +0100

    task 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+""" function returns the dictionary description with simple data structure
+    (list, dictionary, string, integer and boolean)
+    for JSON serialization of an object
+"""
+import json
+
+
+def class_to_json(obj):
+    """
+
+    :param obj: is an instance of a Class
+    :return: the dictionary description with simple data structure
+    """
+    return obj.__dict__
