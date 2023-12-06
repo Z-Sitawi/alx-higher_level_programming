2ffee9020eaa9b57b3e123607a1e5fa90c4489c8
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-12-06 18:03:13 +0100

    task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+""" Define a class named 'Student' """
+
+
+class Student:
+    """ represents a students by
+        first_name, last_name, and age
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """ Initializes a student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ retrieves a dictionary representation of a Student instance """
+        return self.__dict__
