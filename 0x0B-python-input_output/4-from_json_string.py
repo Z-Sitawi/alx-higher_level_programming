f3beb122b5e5c4d70266e393a6a40ad84ae33272
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-12-06 14:11:14 +0100

    task 4

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+""" function that returns an object (Python data structure)
+    represented by a JSON string:
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+
+    :param my_str: a JSON string
+    :return: an object
+    """
+    return json.loads(my_str)
