343c27a88e780ec38aa30ff410faa1c522014d97
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-12-09 00:29:50 +0100

    task 100

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Defines a function that inserts a line of text to a file,
+    after each line containing a specific string
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+
+    :param filename: (str) the name of text file
+    :param search_string: (str) string to look for
+    :param new_string: (str) string to insert
+    :return: nothing
+    """
+    with open(filename, "r+", encoding="utf-8") as file:
+        read = file.read()
+        mylist = []
+        line = ""
+        new_lines = ""
+
+        for i in read:
+            if i != "\n":
+                line += i[:]
+            else:
+                mylist.append(line)
+                line = ""
+        mylist.append(line)
+        for x in mylist:
+            if search_string in x:
+                new_lines += x + "\n" + new_string + "\n"
+            else:
+                new_lines += x + "\n"
+        new_lines = new_lines[: len(new_lines) - 1]
+    with open(filename, "w", encoding="utf-8") as file:
+        file.write(new_lines)
