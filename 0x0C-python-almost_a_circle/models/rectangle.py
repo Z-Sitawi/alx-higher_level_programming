4193221e58db9593feb979e1601e31d273a1bbb8
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-12-12 14:53:52 +0100

    task 2 First Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ Defines a class named Rectangle """
-Base = __import__("base").Base
-
+from models.base import Base
 
 class Rectangle(Base):
     """ Represent a Rectangle """
