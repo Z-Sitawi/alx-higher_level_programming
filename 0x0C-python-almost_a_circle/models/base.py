5ccbd9897a220e675ba8cbef095a330128e86979
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-12-12 04:34:21 -0800

    task 1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Defines a class which will be the “base” of
+    all the other classes in this project
+"""
+
+
+class Base:
+    """ Represents a base """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ Initiates a base """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += Base.__nb_objects
+            self.id = Base.__nb_objects
