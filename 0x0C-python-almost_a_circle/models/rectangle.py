898cf07617e5512aa6bdd89021b5f0f991cd5e5a
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-12-12 17:40:49 +0100

    task 3 Validate attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -27,6 +27,10 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -35,6 +39,10 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -43,6 +51,8 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -51,4 +61,6 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
