7752f780fde7eda1709ce706c9a85573f06b9023
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-12-16 12:35:25 +0100

    task 2 Say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -15,7 +15,7 @@ def say_my_name(first_name, last_name=""):
     elif type(last_name) is not str:
         raise TypeError('last_name must be a string')
 
-    if (last_name is None) or (last_name == ""):
+    if last_name == "":
         print("My name is {:s}".format(first_name))
-    elif (last_name is not None) and (last_name != ""):
+    elif last_name != "":
         print("My name is {:s} {:s}".format(first_name, last_name))
