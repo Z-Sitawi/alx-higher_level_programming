db87936c5ca5192e07560eea07b84c2164201336
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-12-16 12:28:50 +0100

    task 2 Say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,2 +1,21 @@
 #!/usr/bin/python3
-"""to be done"""
+"""Defines a function that prints My name is <first name> <last name> """
+
+
+def say_my_name(first_name, last_name=""):
+    """
+
+    :param first_name: the firs name
+    :param last_name: the lastname
+
+    :raises TypeError: if first_name and last_name are not strings
+    """
+    if type(first_name) is not str:
+        raise TypeError('first_name must be a string')
+    elif type(last_name) is not str:
+        raise TypeError('last_name must be a string')
+
+    if (last_name is None) or (last_name == ""):
+        print("My name is {:s}".format(first_name))
+    elif (last_name is not None) and (last_name != ""):
+        print("My name is {:s} {:s}".format(first_name, last_name))
