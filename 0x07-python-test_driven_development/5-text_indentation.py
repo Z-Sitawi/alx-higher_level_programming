de8a218064058312f2fe5ada6792a3404b96e0e5
Z-Sitawi <aaichaou.zakaria@gmail.com>
2023-12-18 12:54:00 +0100

    task 4 Text indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+""" A function that prints a text with 2 new lines
+    after each of these characters: (.) (?) (:)
+"""
+
+
+def text_indentation(text):
+    """
+
+    :param text: (str) text to print.
+    :raise TypeError: if text is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError('text must be a string')
+    modified_text = ""
+    for i, char in enumerate(text):
+        if char in ['?', ':', '.']:
+            modified_text += char + "\n\n"
+        else:
+            modified_text += char
+
+    print(modified_text.replace("\n\n ", "\n\n"))
