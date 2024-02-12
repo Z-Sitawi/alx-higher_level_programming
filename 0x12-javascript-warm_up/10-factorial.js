2df08c44e0979356d841d4847c21ca25cdab469b
Z-Sitawi <aaichaou.zakaria@gmail.com>
2024-02-12 22:07:55 +0100

    task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 function factorial (num) {
-    num = Number(num)
-    if (num === 0 || isNaN(num)){
-        return 1;
-    }
-    else return num * factorial(num - 1)
+  num = Number(num);
+  if (num === 0 || isNaN(num)) {
+    return 1;
+  } else return num * factorial(num - 1);
 }
 
 console.log(factorial(process.argv[2]));
