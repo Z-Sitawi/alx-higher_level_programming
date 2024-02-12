a68a6dae74068000f4aca8cd46e8d3c6dd3e9f1b
Z-Sitawi <aaichaou.zakaria@gmail.com>
2024-02-12 20:26:36 +0100

    task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,11 +2,7 @@
 const firstArg = parseInt(process.argv[2]);
 if (isNaN(firstArg)) {
   console.log('Missing number of occurrences');
-}
-else if (firstArg <= 0) {
-  return;
-}
-else {
+} else {
   for (let i = 0; i < firstArg; i++) {
     console.log('C is fun');
   }
