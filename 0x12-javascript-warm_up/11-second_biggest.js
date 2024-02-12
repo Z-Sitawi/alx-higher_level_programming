8b1f6a64e42095b045e8e5d9256fd779f4a2e2f2
Z-Sitawi <aaichaou.zakaria@gmail.com>
2024-02-12 22:33:33 +0100

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+const argvArray = process.argv;
+let secondBiggest;
+const newArray = [];
+if (argvArray.length <= 3) {
+  secondBiggest = 0;
+} else {
+  for (let i = 2; i < argvArray.length; i++) {
+    newArray[i - 2] = parseInt(argvArray[i]);
+  }
+  newArray.sort((a, b) => b - a);
+  secondBiggest = newArray[1];
+}
+console.log(secondBiggest);
