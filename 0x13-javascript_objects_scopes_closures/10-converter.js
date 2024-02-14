d3db5b007dd259d3acaa0c602248abb41097cc9f
Z-Sitawi <aaichaou.zakaria@gmail.com>
2024-02-14 14:26:31 +0100

    task 10

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,14 +1,2 @@
 #!/usr/bin/node
-exports.converter = function (base) {
-  const convert = (number, base) => {
-    if (number < base) {
-      return number < 10 ? String(number) : String.fromCharCode(55 + number);
-    }
-
-    return convert(Math.floor(number / base), base) + convert(number % base, base);
-  };
-
-  return function (number) {
-    return convert(number, base);
-  };
-};
+exports.converter = function (base) { return num => num.toString(base); };
