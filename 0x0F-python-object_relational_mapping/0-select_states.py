d80592bd25b7fdcf918d99784e4f1ef4d5f1d9ba
Z-Sitawi <aaichaou.zakaria@gmail.com>
2024-02-18 21:59:09 +0100

    task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,37 @@
 #!/usr/bin/python3
-# Lists all states from the database hbtn_0e_0_usa.
-# Usage: ./0-select_states.py <mysql username> \
-#                             <mysql password> \
-#                             <database name>
+"""
+Lists all states from the database hbtn_0e_0_usa.
+Usage: ./0-select_states.py <mysql username> <mysql password> <database name>
+"""
 import sys
 import MySQLdb
 
+
+def list_states(username, password, database):
+    """
+    Lists all states from the database.
+
+    Args:
+        username (str): MySQL username.
+        password (str): MySQL password.
+        database (str): Name of the MySQL database.
+
+    Returns:
+        None
+    """
+    try:
+        db = MySQLdb.connect(user=username, passwd=password, db=database)
+        c = db.cursor()
+        c.execute("SELECT * FROM `states`")
+        [print(state) for state in c.fetchall()]
+    except MySQLdb.Error as e:
+        print("MySQL Error:", e)
+
+
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall()]
+    if len(sys.argv) != 4:
+        print("Usage: ./0-select_states.py <mysql username> <mysql password> <database name>")
+        sys.exit(1)
+
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    list_states(username, password, database)
