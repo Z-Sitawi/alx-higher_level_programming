2a52db053d08565026ebbb34bd750b7fc6538fb6
Z-Sitawi <aaichaou.zakaria@gmail.com>
2024-02-23 15:07:21 +0100

    task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,38 @@
 #!/usr/bin/python3
+"""
+Lists all states with a name starting with N from the database hbtn_0e_0_usa.
+Usage: ./0-select_states.py <mysql username> <mysql password> <database name>
+"""
+import sys
+import MySQLdb
+
+
+def list_states(username, password, database):
+    """
+    Lists all states from the database.
+
+    Args:
+        username (str): MySQL username.
+        password (str): MySQL password.
+        database (str): Name of the MySQL database.
+
+    Returns:
+        None
+    """
+    try:
+        db = MySQLdb.connect(user=username, passwd=password, db=database)
+        c = db.cursor()
+        c.execute("SELECT * FROM `states` WHERE name LIKE 'N%'")
+        [print(state) for state in c.fetchall()]
+    except MySQLdb.Error as e:
+        print("MySQL Error:", e)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: ./0-select_states.py <mysql username> <mysql password> "
+              "<database name>")
+        sys.exit(1)
+
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    list_states(username, password, database)
