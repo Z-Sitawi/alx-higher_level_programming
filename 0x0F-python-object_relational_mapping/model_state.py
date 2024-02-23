2fc120ebe7b89997f51bbde3977a2763b0787793
Z-Sitawi <aaichaou.zakaria@gmail.com>
2024-02-23 23:08:04 +0100

    fix task 7

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -19,3 +19,6 @@ class State(Base):
 
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
+
+    def __str__(self):
+        return f"{State.id}: {State.name}"
