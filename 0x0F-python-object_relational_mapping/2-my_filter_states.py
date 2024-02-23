7aff32a944270aa2a02b9532bb1641dabc6f7fde
Z-Sitawi <aaichaou.zakaria@gmail.com>
2024-02-23 15:25:56 +0100

    task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""
+Displays all values in the states table of hbtn_0e_0_usa
+where name matches the 4th argument.
+Usage: ./0-select_states.py
+<mysql username> <mysql password> <database name> <state name searched>
+"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states` WHERE name = {} ORDER BY `id`".format(sys.argv[4]))
+    states = c.fetchall()
+    for state in states:
+        print(state)
