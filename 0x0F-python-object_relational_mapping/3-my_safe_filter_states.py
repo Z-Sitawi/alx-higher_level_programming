baa2a79b5ca128d043730b4b63cf1beb4a5150d7
Z-Sitawi <aaichaou.zakaria@gmail.com>
2024-02-23 15:46:01 +0100

    task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,24 @@
-#!/usr/bin/python3
+#!/usr/bin/#!/usr/bin/python3
+"""
+This script is safe from MySQL injections:
+Displays all values in the states table of hbtn_0e_0_usa
+where name matches the 4th argument.
+Usage: ./0-select_states.py
+<mysql username> <mysql password> <database name> <state name searched>
+"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+
+    # Execute a parameterized query
+    sql_query = "SELECT * FROM states WHERE name = %s ORDER BY id"
+    search_query = (sys.argv[4],)
+    c.execute(sql_query, search_query)
+
+    states = c.fetchall()
+    for state in states:
+        print(state)
