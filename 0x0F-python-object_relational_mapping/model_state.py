be7da4f9a2100210f81df7a86cbd9bd94aab5cef
Z-Sitawi <aaichaou.zakaria@gmail.com>
2024-02-23 19:12:37 +0100

    task 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""
-class definition of a State and an instance Base = declarative_base():
-"""
+# Defines a State model.
+# Inherits from SQLAlchemy Base and links to the MySQL table states.
 from sqlalchemy.orm import declarative_base
 from sqlalchemy import String, Integer, Column
 
@@ -9,6 +8,11 @@ Base = declarative_base()
 
 
 class State(Base):
+    """Represents a state for a MySQL database.
+        __tablename__ (str): The name of the MySQL table to store States.
+        id (sqlalchemy.Integer): The state's id.
+        name (sqlalchemy.String): The state's name.
+    """
     __tablename__ = "states"
 
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
