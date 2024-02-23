7ef71f9c0d4ec03f6b860bd6af03c415e68b737f
Z-Sitawi <aaichaou.zakaria@gmail.com>
2024-02-23 15:17:51 +0100

    task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,38 +7,8 @@ import sys
 import MySQLdb
 
 
-def list_states(username, password, database):
-    """
-    Lists all states from the database.
-
-    Args:
-        username (str): MySQL username.
-        password (str): MySQL password.
-        database (str): Name of the MySQL database.
-
-    Returns:
-        None
-    """
-    try:
-        db = MySQLdb.connect(user=username, passwd=password, db=database)
-        c = db.cursor()
-        c.execute("SELECT * FROM `states` WHERE name LIKE 'N%' ORDER BY id ASC")
-        # Fetch all rows
-        states = c.fetchall()
-        # Display results
-        for state in states:
-            print(state)
-        c.close()
-        db.close()
-    except MySQLdb.Error as e:
-        print("MySQL Error:", e)
-
-
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: ./0-select_states.py <mysql username> <mysql password> "
-              "<database name>")
-        sys.exit(1)
-
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
-    list_states(username, password, database)
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in c.fetchall() if state[1][0] == "N"]
