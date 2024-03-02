e9cca4484c0e5e2b0d897b8449adb11b99f4e05e
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-03-02 10:04:39 -0800

    task

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL and displays all HTTP methods the server will accept.
-curl -sIX OPTIONS "$1" | grep "Allow:" | awk '{print $2}'
+curl -sI  "$1" | sed -n '/Allow: /s/Allow: //p'
