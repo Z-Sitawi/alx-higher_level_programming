f6d342c990f98004e087d522e0d088e78896a1aa
Z-Sitawi <aaichaou.zakaria@gmail.com>
2024-03-03 14:17:08 +0100

    task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+""" Takes in a URL, sends a request to the URL
+and displays the body of the response (decoded in utf-8).
+Usage: ./3-error_code.pu <url>
+"""
+import sys
+import urllib.request
+import urllib.error
+
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    request = urllib.request.Request(url)
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read().decode('utf-8')
+            print(body)
+    except urllib.error.HTTPError as e:
+        print(e.code)
