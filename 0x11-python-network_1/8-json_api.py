5c5284fe191dea4ee7df1a85a7f74f6ae70c8ba7
Z-Sitawi <aaichaou.zakaria@gmail.com>
2024-03-03 16:54:44 +0100

    task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""Sends a POST request to http://0.0.0.0:5000/search_user with a given letter.
+
+Usage: ./8-json_api.py <letter>
+  - The letter is sent as the value of the variable `q`.
+  - If no letter is provided, sends `q=""`.
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    letter = sys.argv[1]
+    url = 'http://0.0.0.0:5000/search_user'
+    if letter is None:
+        letter = ""
+    param = {'q': letter}
+    response = requests.get(url, data=param)
+    try:
+        json_resp = response.json()
+        if json_resp is None:
+            print('No result')
+        else:
+            print(f"[{json_resp['id']}] {json_resp['name']}")
+    except Exception:
+        print("Not a valid JSON")
