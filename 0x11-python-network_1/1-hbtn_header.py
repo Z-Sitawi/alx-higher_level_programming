c8691597919c970280be21fe64f9ec965de86e26
Z-Sitawi <aaichaou.zakaria@gmail.com>
2024-03-03 13:28:15 +0100

    fix doc

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ A Python script that takes in a URL as 1st arg, sends a request to the URL
-    and displays the value of the X-Request-Id variable 
-    found in the header of the response.
+and displays the value of the X-Request-Id variable
+found in the header of the response.
+
+Usage: ./1-hbtn_header.py <URL>
 """
 import urllib.request
 from sys import argv
