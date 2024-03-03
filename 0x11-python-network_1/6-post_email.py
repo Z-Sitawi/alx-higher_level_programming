3b9211c587ff858c92bcc046180ddaa6fc04e4ef
Z-Sitawi <aaichaou.zakaria@gmail.com>
2024-03-03 15:00:58 +0100

    task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Sends a POST request to a given URL with a given email.
+
+Usage: ./6-post_email.py <URL> <email>
+  - Displays the body of the response.
+"""
+import sys
+import requests
+
+
+if __name__ == '__main__':
+    url, email = sys.argv[1], {'email': sys.argv[2]}
+    response = requests.get(url, data=email)
+    print(response.text)
