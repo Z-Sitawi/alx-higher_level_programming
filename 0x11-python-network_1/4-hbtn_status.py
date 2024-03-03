8f34ec277b78e674475b8b42dbcf6f57c5206539
Z-Sitawi <aaichaou.zakaria@gmail.com>
2024-03-03 14:32:08 +0100

    task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+fetches https://alx-intranet.hbtn.io/status
+"""
+import requests
+
+
+if __name__ == '__main__':
+    request = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
+    print(f'\t- type: {type(request.text)}')
+    print(f'\t- content: {request.content}')
