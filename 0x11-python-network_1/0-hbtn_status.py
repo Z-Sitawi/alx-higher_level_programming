51785f5b68483773d3dd059c5339c7cbaa498475
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-03-03 03:56:57 -0800

    task

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+# A Python script that fetches https://alx-intranet.hbtn.io/status.
+
+
+import urllib.request
+
+
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as rsp:
+        bodyResponse = rsp.read()
+        print('Body response:')
+        print(f'\t- type: {type(bodyResponse)}')
+        print(f'\t- content: {bodyResponse}')
+        print(f'\t- utf8 content: {bodyResponse.decode("utf-8")}')
