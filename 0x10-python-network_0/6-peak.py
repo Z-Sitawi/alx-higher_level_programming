b4c005ba8b83e62e02dcd1c70b7ab1157b6fd440
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-03-03 11:57:21 -0800

    fix task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,33 +3,21 @@
 
 
 def find_peak(list_of_integers):
-    """
-        Finds a peak in a list of unsorted integers.
-
-    :param list_of_integers: list of unsorted integers
-    :return: The peak found / list of the peaks / None if no peak found.
-    """
-    lint = list_of_integers
-    length = len(lint)
-    if length == 0:
+    """Return a peak in a list of unsorted integers."""
+    if list_of_integers == []:
         return None
 
-    if length == 1:
-        return lint[0]
+    size = len(list_of_integers)
+    if size == 1:
+        return list_of_integers[0]
+    elif size == 2:
+        return max(list_of_integers)
 
-    peaks = []
-    if length == 2:
-        if lint[0] > lint[1]:
-            peaks.append(lint[0])
-        else:
-            peaks.append(lint[1])
+    mid = int(size / 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
     else:
-        if lint[0] > lint[1]:
-            peaks.append(lint[0])
-        if lint[-1] > lint[-2]:
-            peaks.append(lint[-1])
-
-        for n in range(1, length - 1):
-            if lint[n-1] <= lint[n] >= lint[n+1]:
-                peaks.append(lint[n])
-    return find_peak(peaks)
+        return find_peak(list_of_integers[mid + 1:])
