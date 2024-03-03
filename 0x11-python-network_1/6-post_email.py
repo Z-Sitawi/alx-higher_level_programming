ea671c3cb1e63260af90a15194ebd1dff4ed8ac4
Z-Sitawi <aaichaou.zakaria@gmail.com>
2024-03-03 15:02:34 +0100

    task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -8,7 +8,9 @@ import sys
 import requests
 
 
-if __name__ == '__main__':
-    url, email = sys.argv[1], {'email': sys.argv[2]}
-    response = requests.get(url, data=email)
-    print(response.text)
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    params = {"email": email}
+    res = requests.post(url, data=params)
+    print(res.text)
