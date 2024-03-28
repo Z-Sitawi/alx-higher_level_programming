4ca892ac13fc33fefb42cd71ac23a368c7598fe6
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-03-28 15:14:21 +0000

    status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+/* Display the status code of a GET request. */
+if (process.argv.length > 2){
+    const url = process.argv[2];
+    const request = require('request');
+    request(url, function (error, response){
+        console.log('code:', response.statusCode);
+    })
+}
