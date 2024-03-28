ac94b1a6e0951155c1b54ebc8eb2fedf172fb69e
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-03-28 18:17:04 +0000

    advanced task 1

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,1 +1,27 @@
 #!/usr/bin/node
+/* prints all characters of a Star Wars movie
+based on a given Movie id
+Usage: ./100-starwars_charachter.js <movieId (int)> */
+
+if (process.argv.length > 2) {
+  const movieId = process.argv[2];
+  const request = require('request');
+  const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
+  request(url, function (error, response) {
+    if (error) {
+      console.log(error);
+    } else {
+      const characters = JSON.parse(response.body).characters;
+      for (const character of characters) {
+        const cUrl = character + '?format=json';
+        request(cUrl, function (error, res, body) {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log(JSON.parse(body).name);
+          }
+        });
+      }
+    }
+  });
+}
