1828ccc1aa92e482555370896bbf54c6a7d66024
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-03-28 18:45:09 +0000

    advanced task 2

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 /* prints all characters of a Star Wars movie
-based on a given Movie id
+based on a given Movie id (sorted)
 Usage: ./100-starwars_charachter.js <movieId (int)> */
 
 const request = require('request');
@@ -13,15 +13,37 @@ request.get(url, (error, response, body) => {
   } else {
     const content = JSON.parse(body);
     const characters = content.characters;
-    for (const character of characters) {
-      request.get(character, (error, response, body) => {
-        if (error) {
-          console.log(error);
-        } else {
-          const names = JSON.parse(body);
-          console.log(names.name);
-        }
+
+    const getNumberFromUrl = (url) => {
+      const parts = url.split('/');
+      return parseInt(parts[parts.length - 2]);
+    };
+
+    // Sort the characters array based on the number in the URL
+    characters.sort((a, b) => {
+      const numberA = getNumberFromUrl(a);
+      const numberB = getNumberFromUrl(b);
+      return numberA - numberB;
+    });
+
+    // Function to fetch character details and print them sequentially
+    const fetchAndPrintCharacter = (characterUrl) => {
+      return new Promise((resolve, reject) => {
+        request.get(characterUrl, (error, response, body) => {
+          if (error) {
+            reject(error);
+          } else {
+            const character = JSON.parse(body);
+            console.log(character.name);
+            resolve();
+          }
+        });
       });
-    }
+    };
+
+    // Sequentially fetch and print each character
+    characters.reduce((promiseChain, characterUrl) => {
+      return promiseChain.then(() => fetchAndPrintCharacter(characterUrl));
+    }, Promise.resolve());
   }
 });
