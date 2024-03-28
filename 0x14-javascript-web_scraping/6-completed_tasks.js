1e9166624c4b878962cdf7ad29dc595396b21301
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-03-28 17:32:10 +0000

    completed tasks

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,25 @@
 #!/usr/bin/node
+/*  computes the number of tasks completed by user id. */
+if (process.argv.length > 2) {
+  const url = process.argv[2];
+  const request = require('request');
+  request(url, function (error, response) {
+    if (error) {
+      console.log(error);
+    } else {
+      const body = JSON.parse(response.body);
+      const completedUid = [];
+      for (const user of body) {
+        if (user.completed) {
+          completedUid.push(user.userId);
+        }
+      }
+      const result = completedUid.reduce((acc, currentValue) => {
+        acc[currentValue] = (acc[currentValue] || 0) + 1;
+        return acc;
+      }, {});
+
+      console.log(result);
+    }
+  });
+}
