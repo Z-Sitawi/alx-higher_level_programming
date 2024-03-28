fe766206ddb312f5a027fa0939b3674e7bae53c8
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-03-28 15:36:01 +0000

    star wars api

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+/* prints the title of a Star Wars movie where
+the episode number matches a given integer. */
+if (process.argv.length > 2) {
+  const movieId = process.argv[2];
+  const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
+  const request = require('request');
+  request(url, function (error = null, response) {
+    console.log(JSON.parse(response.body).title);
+  });
+}
