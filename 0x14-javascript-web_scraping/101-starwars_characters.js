7e8b3e6f3a7f383db34dd2cd31a445960e2580c8
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-03-28 18:26:53 +0000

    advanced task 2

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,25 +3,26 @@
 based on a given Movie id
 Usage: ./100-starwars_charachter.js <movieId (int)> */
 
-if (process.argv.length > 2) {
-  const movieId = process.argv[2];
-  const request = require('request');
-  const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
-  request(url, function (error, response) {
-    if (error) {
-      console.log(error);
-    } else {
-      const characters = JSON.parse(response.body).characters;
-      for (const character of characters) {
-        const cUrl = character + '?format=json';
-        request(cUrl, function (error, res, body) {
-          if (error) {
-            console.log(error);
-          } else {
-            console.log(JSON.parse(body).name);
-          }
-        });
-      }
+const request = require('request');
+const id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const content = JSON.parse(body);
+    const characters = content.characters;
+
+    for (const character of characters) {
+      request.get(character, (error, response, body) => {
+        if (error) {
+          console.log(error);
+        } else {
+          const names = JSON.parse(body);
+          console.log(names.name);
+        }
+      });
     }
-  });
-}
+  }
+});
