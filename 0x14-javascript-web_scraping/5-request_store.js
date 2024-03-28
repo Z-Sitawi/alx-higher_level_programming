40fab2280ef4023913eb3bdc48abd1a55f006b64
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-03-28 17:01:20 +0000

    loripsum

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+/* gets the contents of a webpage and stores it in a file. */
+if (process.argv.length > 2) {
+  const url = process.argv[2];
+  const path = process.argv[3];
+  const request = require('request');
+  const fs = require('fs');
+  request(url, function (error, response, body) {
+    if (error) { console.log(error); } else {
+      fs.writeFile(path, body, 'utf-8', (err) => {
+        if (err) { console.log(err); }
+      });
+    }
+  });
+}
