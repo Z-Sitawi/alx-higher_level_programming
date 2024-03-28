aa033b3c95e63fb32a0b0ee1c78239ab5abdc994
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-03-28 07:56:47 +0000

    write to a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+/* writes a string to a file. */
+const argvLength = process.argv.length;
+if (argvLength > 3) {
+  const fs = require('fs');
+  const file = process.argv[2];
+  const content = process.argv[3];
+
+  fs.writeFile(file, content, (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+}
