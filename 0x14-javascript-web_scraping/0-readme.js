6e36149cd52eae7fbbb4217794636f596f6e6f2f
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-03-28 07:51:27 +0000

    read data from a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+/* reads and prints the content of a file. */
+const argvLength = process.argv.length;
+if (argvLength > 2) {
+  const fs = require('fs');
+  const file = process.argv[2];
+
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(data);
+  });
+}
