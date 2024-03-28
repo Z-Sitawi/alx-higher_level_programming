6739141b5ca96726678a8b985b7daff53aadc50c
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-03-28 16:10:38 +0000

    Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+/* prints the number of movies where
+the character “Wedge Antilles” is present. */
+if (process.argv.length > 2) {
+  const url = process.argv[2];
+  let count = 0;
+  const wedge = 'https://swapi-api.alx-tools.com/api/people/18/';
+  const request = require('request');
+  request(url, function (error = null, response) {
+    const films = JSON.parse(response.body).results;
+    for (const film of films) {
+      for (const character of film.characters) {
+        if (character === wedge) { count++; }
+      }
+    }
+    console.log(count);
+  });
+}
