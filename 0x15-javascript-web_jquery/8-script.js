4a079e1a78907c63a421315fad974e3cc607a779
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-04-03 22:00:01 +0000

    task 8

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+
+const url = 'https://swapi-api.alx-tools.com/api/films/?format=json';
+fetch(url)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    const allFilms = data.results
+    allFilms.forEach(film => {
+        $('#list_movies').append("<li>"+film.title+"</li>")
+    });
+  })
+  .catch(error => {
+    console.log('Error fetching data:', error);
+  });
