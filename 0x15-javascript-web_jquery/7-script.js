5c917737ad68691b0183e8658061fb4898994c1c
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-04-03 21:46:31 +0000

    task 7

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+fetch('https://swapi-api.alx-tools.com/api/people/5/?format=json')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    $('#character').text(data.name);
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
