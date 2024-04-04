7ea07f4e995ecc869ad920cece2b0a80c490b0d7
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-04-04 00:47:47 +0000

    task 102 & 103

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,21 +1,21 @@
-#!/usr/bin/node
-
 document.addEventListener('DOMContentLoaded', function () {
-  $('#btn_translate').click(function (){
-    const lang = $('#language_code').val();
-    let url = 'https://hellosalut.stefanbohacek.dev/?lang='+lang;
-    fetch(url)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response not ok');
-    }
-    return response.json();
-  })
-  .then(data => {
-    $('#hello').text(data.hello);
-  })
-  .catch(error => {
-    console.log('Error fetching data:', error);
+  $('#btn_translate').click(fetchTranslation);
   });
-  })
-});
+
+  function fetchTranslation () {
+    const lang = $('#language_code').val();
+    const url = 'https://hellosalut.stefanbohacek.dev/?lang=' + lang;
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        $('#hello').text(data.hello);
+      })
+      .catch(error => {
+        console.log('Error fetching data:', error);
+      });
+  }
--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -1,1 +1,26 @@
-#!/usr/bin/node
+document.addEventListener('DOMContentLoaded', function () {
+  $('#btn_translate').click(fetchTranslation);
+  $('#language_code').keypress(function (event) {
+    if (event.which === 13) { // Enter key
+      fetchTranslation();
+    }
+  });
+
+  function fetchTranslation () {
+    const lang = $('#language_code').val();
+    const url = 'https://hellosalut.stefanbohacek.dev/?lang=' + lang;
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        $('#hello').text(data.hello);
+      })
+      .catch(error => {
+        console.log('Error fetching data:', error);
+      });
+  }
+});
