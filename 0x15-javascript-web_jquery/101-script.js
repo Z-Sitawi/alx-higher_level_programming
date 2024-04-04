6c319a9bce1cf9f97fc64746281b007086a644a6
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-04-04 00:10:59 +0000

    task 101

--- 0x15-javascript-web_jquery/101-script.js
+++ 0x15-javascript-web_jquery/101-script.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+
+document.addEventListener('DOMContentLoaded', function () {
+  const myList = $('.my_list');
+  const _add = $('#add_item');
+  const _remove = $('#remove_item');
+  const _clear = $('#clear_list');
+
+  _add.click(function () {
+    myList.append('<li>Item</li>');
+  });
+
+  _remove.click(function () {
+    myList.children('li').last().remove();
+  });
+
+  _clear.click(function () {
+    myList.children('li').remove();
+  });
+});
