47e4aed52bca45c1044cba2f9dea8aa471e8c678
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-04-04 00:40:07 +0000

    task 102

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,1 +1,21 @@
 #!/usr/bin/node
+
+document.addEventListener('DOMContentLoaded', function () {
+  $('#btn_translate').click(function (){
+    const lang = $('#language_code').val();
+    let url = 'https://hellosalut.stefanbohacek.dev/?lang='+lang;
+    fetch(url)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    $('#hello').text(data.hello);
+  })
+  .catch(error => {
+    console.log('Error fetching data:', error);
+  });
+  })
+});
